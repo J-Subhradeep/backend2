2065fe0ec60d5bc96ab242b5c471ff46855ced95
ani <subhradeeppal30@gmail.com>
2021-12-17 19:46:44 +0530

    get method

--- blogapp/home/views.py
+++ blogapp/home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import BlogPost
+from django.contrib.auth.models import User
+from rest_framework import status
+from .serializers import BlogPostSerializer
 # Create your views here.
+
+
+class BlogPostView(APIView):
+    def get(self, request, pk=None, format=None):
+        blg = BlogPost.objects.all()
+        if pk:
+            blg = BlogPost.objects.get(pk=pk)
+        serializers = BlogPostSerializer(blg, many=True)
+        return Response(serializers.data, status=status.HTTP_200_OK)
