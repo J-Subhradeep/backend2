94cb11e215e71ae9a82d8307c2d3b0329f8caa03
Pranjal <tataisarkar2001@gmail.com>
2021-12-17 20:07:19 +0530

    User API Done

--- blogapp/home/views.py
+++ blogapp/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import BlogPost
@@ -7,11 +8,24 @@ from rest_framework import status
 from .serializers import BlogPostSerializer
 # Create your views here.
 
+#harry: ##HHHH@12345
 
-class BlogPostView(APIView):
+class UserView(APIView):
     def get(self, request, pk=None, format=None):
         blg = BlogPost.objects.all()
         if pk:
             blg = BlogPost.objects.get(pk=pk)
         serializers = BlogPostSerializer(blg, many=True)
         return Response(serializers.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        data = request.data
+        username = data.get('username')
+        password = data.get('password')
+        email = data.get('email')
+
+        user = User.objects.create_user(username, email, password)
+
+        return Response({
+            'message': 'OK'
+        })        
